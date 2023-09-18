5a47f7c94eb4fd42035fecc7c6495359298db582
CameronJWalker <cameron.jr.walker@icloud.com>
2023-09-17 18:18:23 -0700

    finished profile page, need to fix onboarding appearing even after completion

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,17 +3,21 @@ import Link from "next/link"
 import Image from "next/image";
 import { sidebarLinks } from "@/constants"
 import { usePathname, useRouter } from 'next/navigation';
-import { SignedIn, SignOutButton } from "@clerk/nextjs"
+import { SignedIn, SignOutButton, useAuth } from "@clerk/nextjs"
+
 
 function LeftSidebar() {
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
 
     return (
         <section className="custom-scrollbar leftsidebar">
             <div className="flex w-full flex-1 flex-col gap-6 px-6">
                 {sidebarLinks.map((link) =>  {
                 const isActive = (pathname.includes (link.route) && link.route.length > 1) || pathname === link.route;
+
+                if(link.route === '/profile') link.route = `${link.route}/${userId}`
 
                 return (
                     <Link 
